6d614932e7d1367666ba69dc2c3a655c22413744
pbijampola <patrickbijampola@gmail.com>
2022-01-11 16:23:59 +0300

    Firebase Authentication is ready

--- app/src/main/java/com/example/appquiz/LoginActivity.java
+++ app/src/main/java/com/example/appquiz/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.appquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -12,12 +15,25 @@ import android.widget.Toast;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
     EditText email,password;
-    AwesomeValidation awesomeValidation;
     Button loginbtn;
+    String emailPattern= "[a-zA-Z0-9._-]+@[a-z]+\\._[a-z]+";
+    ProgressDialog progressDialog;
+
+    //instance of firebase
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+   // AwesomeValidation awesomeValidation;
+
 
 
     @Override
@@ -29,27 +45,79 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.login_mail);
         password = findViewById(R.id.login_password);
         loginbtn = findViewById(R.id.loginbtnid);
+        progressDialog = new ProgressDialog(this);
 
-        //initializing the validation style
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        mAuth= FirebaseAuth.getInstance();
+        mUser= mAuth.getCurrentUser();
 
-        //validating email
-        awesomeValidation.addValidation(this,R.id.login_mail, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
-        //validating password
-        awesomeValidation.addValidation(this,R.id.login_password,".{8,}",R.string.invalid_password);
-
-        //Event logging
+//        //initializing the validation style
+//        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+//
+//        //validating email
+//        awesomeValidation.addValidation(this,R.id.login_mail, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+//        //validating password
+//        awesomeValidation.addValidation(this,R.id.login_password,".{8,}",R.string.invalid_password);
+//
+//        //Event logging
+//        loginbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // cheking validation
+//                if(awesomeValidation.validate()){
+//                    Toast.makeText(getApplicationContext(), "Login Successfull", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(), "Failed to Login", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // cheking validation
-                if(awesomeValidation.validate()){
-                    Toast.makeText(getApplicationContext(), "Login Successfull", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Failed to Login", Toast.LENGTH_SHORT).show();
-                }
+                appLogin();
             }
         });
     }
+
+    private void appLogin() {
+
+        String mail = email.getText().toString();
+        String passwd = password.getText().toString();
+
+        if (!mail.matches(emailPattern)) {
+            email.setError("Enter correct E-mai");
+        }
+        if (passwd.isEmpty() || passwd.length() < 8) {
+            password.setError("Enter Correct Password");
+        } else {
+            progressDialog.setMessage("Please Wait While Login");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(mail,passwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToHomeActivity();
+                        Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void sendUserToHomeActivity() {
+        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/appquiz/RegisterActivity.java
+++ app/src/main/java/com/example/appquiz/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.appquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,14 +16,25 @@ import android.widget.Toast;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText username,email,password;
     Button directlog,signupbtn;
+    String emailPattern= "[a-zA-Z0-9._-]+@[a-z]+\\._[a-z]+";
+    ProgressDialog progressDialog;
 
-    AwesomeValidation awesomeValidation;
+    //instance of firebase
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+//    AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +46,36 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.emailid);
         password = findViewById(R.id.passwordid);
         signupbtn = findViewById(R.id.signupid);
+        progressDialog = new ProgressDialog(this);
 
-        // Initializing validation style
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        mAuth= FirebaseAuth.getInstance();
+        mUser= mAuth.getCurrentUser();
 
-        //validating username
-        awesomeValidation.addValidation(this,R.id.usernameid, RegexTemplate.NOT_EMPTY,R.string.invalid_username);
-        //validating email
-        awesomeValidation.addValidation(this,R.id.emailid, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
-        //validating password
-        awesomeValidation.addValidation(this,R.id.passwordid,".{8,}",R.string.invalid_password);
+//        // Initializing validation style
+//        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+//
+//        //validating username
+//        awesomeValidation.addValidation(this,R.id.usernameid, RegexTemplate.NOT_EMPTY,R.string.invalid_username);
+//        //validating email
+//        awesomeValidation.addValidation(this,R.id.emailid, Patterns.EMAIL_ADDRESS,R.string.invalid_email);
+//        //validating password
+//        awesomeValidation.addValidation(this,R.id.passwordid,".{8,}",R.string.invalid_password);
+//        // onclickEvent signupbtn
+//        signupbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //checking for validation
+//                if(awesomeValidation.validate()){
+//                    //Success message
+//                    Toast.makeText(getApplicationContext(), "Your Registration is Successful", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//                    Toast.makeText(getApplicationContext(), "Your Registration is not Successful", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+        //USING FIREBASE AUTHENTICATION
 
-        // onclickEvent signupbtn
-        signupbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //checking for validation
-                if(awesomeValidation.validate()){
-                    //Success message
-                    Toast.makeText(getApplicationContext(), "Your Registration is Successful", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Your Registration is not Successful", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
         directlog=findViewById(R.id.alreadyaccount);
         directlog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +86,54 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAuth();
+            }
+        });
+    }
 
+    private void checkAuth() {
+        // getting the edit fields first
+        String name_=username.getText().toString();
+        String mail=email.getText().toString();
+        String passwd=password.getText().toString();
+
+        if(!mail.matches(emailPattern)){
+            email.setError("Enter correct E-mai");
+        }
+        if(passwd.isEmpty()||passwd.length()<8){
+            password.setError("Enter Correct Password");
+        }else{
+            progressDialog.setMessage("Please Wait While Registration");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            //creating a user
+            mAuth.createUserWithEmailAndPassword(mail,passwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToLoginActivity();
+                        Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
